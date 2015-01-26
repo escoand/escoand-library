38dd7551ca3208825f75de7d5c39a203e8510f6a
escoand <escoand@users.noreply.github.com>
2015-01-26 20:33:21 +0100

    renamed classpath

--- src/de/escoand/android/library/OnSwipeTouchListener.java
+++ src/de/escoand/android/library/OnSwipeTouchListener.java
@@ -13,7 +13,7 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
-package com.escoand.android.library;
+package de.escoand.android.library;
 
 import android.content.Context;
 import android.view.GestureDetector;
